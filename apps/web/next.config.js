724ad567e01191237a3169f02a1abc7556a1605a
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2024-07-28 17:59:35 +0300

    fix prisma in nextjs

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,6 @@
 import { fileURLToPath } from "url";
+// @ts-ignore
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 import createJiti from "jiti";
 
 // Import env files to validate at build time. Use jiti so we can load .ts files in here.
@@ -14,6 +16,14 @@ const config = {
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
+  },
 };
 
 export default config;
