35d1cc7c4bb64de02441baf2940c0fdca39734c8
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2024-07-28 17:54:38 +0300

    use redis url

--- apps/queue/src/lib/redis.ts
+++ apps/queue/src/lib/redis.ts
@@ -3,12 +3,6 @@ import Redis from "ioredis";
 import { env } from "../env";
 
 export const createRedis = () =>
-  new Redis({
-    port: env.REDIS_PORT,
-    host: env.REDIS_HOST,
-    password: env.REDIS_PASSWORD,
-    username: env.REDIS_USERNAME,
-    tls: {
-      rejectUnauthorized: false,
-    },
+  new Redis(env.REDIS_URL, {
+    maxRetriesPerRequest: null,
   });
