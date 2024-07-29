d30929a74fcbdc4a03e685883dd9369d531d4869
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2024-07-29 17:40:46 +0300

    add cors middleware

--- apps/queue/src/app.ts
+++ apps/queue/src/app.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { compress } from "hono/compress";
+import { cors } from "hono/cors";
 import { secureHeaders } from "hono/secure-headers";
 
 import routes from "./routes";
@@ -11,6 +12,7 @@ const app = new Hono();
 
 app.use(secureHeaders());
 app.use(compress());
+app.use(cors());
 
 app.route("/", routes);
 
