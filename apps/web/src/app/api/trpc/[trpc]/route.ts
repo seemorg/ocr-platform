d4dd5d43f054f632a34598edb7712f10d708d23f
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2024-11-29 18:22:15 +0200

    make onError for all environments

--- apps/web/src/app/api/trpc/[trpc]/route.ts
+++ apps/web/src/app/api/trpc/[trpc]/route.ts
@@ -1,9 +1,7 @@
-import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { type NextRequest } from "next/server";
-
-import { env } from "@/env";
 import { appRouter } from "@/server/api/root";
 import { createTRPCContext } from "@/server/api/trpc";
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 
 /**
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
@@ -21,14 +19,11 @@ const handler = (req: NextRequest) =>
     req,
     router: appRouter,
     createContext: () => createContext(req),
-    onError:
-      env.NODE_ENV === "development"
-        ? ({ path, error }) => {
-            console.error(
-              `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
-            );
-          }
-        : undefined,
+    onError: ({ path, error }) => {
+      console.error(
+        `❌ tRPC error on ${path ?? "<no-path>"}: ${error.message}`,
+      );
+    },
   });
 
 export { handler as GET, handler as POST };
