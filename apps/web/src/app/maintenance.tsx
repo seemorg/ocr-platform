a4fa950206d2cbf1c30438c1793980b2a5f36469
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2024-12-05 03:06:13 +0200

    new page

--- apps/web/src/app/maintenance.tsx
+++
@@ -1,7 +0,0 @@
-export default function Maintenance() {
-  return (
-    <div className="flex min-h-screen items-center justify-center text-center text-2xl">
-      Under Maintenance
-    </div>
-  );
-}
---
+++ apps/web/src/app/maintenance/page.tsx
@@ -0,0 +1,7 @@
+export default function Maintenance() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-center text-4xl font-bold">Under Maintenance</h1>
+    </div>
+  );
+}
