86d23b5e41bd68943d11ccd0c103bbd9cf3e7259
Anas Ahmed <anas.ahassan@yahoo.com>
2026-02-02 13:52:44 +0200

    update: redis reconnection

--- apps/queue/src/lib/redis.ts
+++ apps/queue/src/lib/redis.ts
@@ -5,4 +5,13 @@ import { env } from "../env";
 export const createRedis = () =>
   new Redis(env.REDIS_URL, {
     maxRetriesPerRequest: null,
+    tls: {
+      rejectUnauthorized: false,
+    },
+    connectTimeout: 10000,
+    keepAlive: 30000,
+    retryStrategy: (times) => {
+      const delay = Math.min(times * 50, 2000);
+      return delay;
+    },
   });
