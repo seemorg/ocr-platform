9b04c05162310ac95810ef59dbb1dd1111cb9ecd
Anas Ahmed <anas.ahassan@yahoo.com>
2026-02-02 16:28:11 +0200

    update: change ai endpoint `openai` to `cognitiveservices`

--- apps/web/src/lib/openai/index.ts
+++ apps/web/src/lib/openai/index.ts
@@ -4,6 +4,6 @@ import { AzureOpenAI } from "openai";
 export const openai = new AzureOpenAI({
   deployment: env.AZURE_OPENAI_DEPLOYMENT_NAME,
   apiVersion: "2024-10-21",
-  endpoint: `https://${env.AZURE_OPENAI_RESOURCE_NAME}.openai.azure.com/`,
+  endpoint: `https://${env.AZURE_OPENAI_RESOURCE_NAME}.cognitiveservices.azure.com/`,
   apiKey: env.AZURE_OPENAI_KEY,
 });
